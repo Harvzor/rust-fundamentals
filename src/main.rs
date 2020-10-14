aa97c7d0812bc6796ad2fe2aebf5fc4bea86b4f1
Harvey <harveyjwilliams@aol.co.uk>
2020-10-14 21:18:18 +0200

    2. Stack and heap.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,44 @@
-// It's basically inserted into the rest of the application.
-// There's no fixed address.
-const MEANING_OF_LIFE: u8 = 42;
+#![allow(dead_code)]
 
-// Actually has an address.
-// Isn't normally mutable because any part of the application could read/write the var and break memory safety.
-static mut Z: i32 = 123;
+use std::mem;
+
+// Stack is fast but limited.
+// There's plenty of stack.
+
+struct Point
+{
+    x: f64,
+    y: f64,
+}
+
+fn origin() -> Point
+{
+    Point
+    {
+        x: 0.0,
+        y: 0.0,
+    }
+}
+
+fn stack_and_heap() {
+    // Stack allocated by default.
+    let p1 = origin();
+
+    // Pointer to location on the stack.
+    let p2 = Box::new(origin());
+
+    // Should be 16 bytes since there's 2 f64 vars.
+    println!("p1 takes up {} bytes", mem::size_of_val(&p1));
+
+    // Should be 8 since this is a 8 byte system.
+    println!("p2 takes up {} bytes", mem::size_of_val(&p2));
+
+    // Follow the point, relocate back to the stack in a new var.
+    let p3 = *p2;
+
+    println!("{}", p3.x);
+}
 
 fn main() {
-    println!("meaning of life =  {}", MEANING_OF_LIFE);
-
-    // I promise to not mess up Z
-    unsafe
-    {
-        Z = 777;
-        println!("Z =  {}", Z);
-    }
+    stack_and_heap();
 }
