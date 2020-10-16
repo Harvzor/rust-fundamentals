c0ba99e2fc9e2dcb096dee3110e51c88b2a0a41e
Harvey <harveyjwilliams@aol.co.uk>
2020-10-16 23:04:36 +0200

    4. Enumerations

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,31 @@
-struct Point {
-    x: f64,
-    y: f64,
+use std::mem;
+
+enum Color {
+    Red,
+    Green,
+    Blue,
+    // Tuple
+    RgbColor(u8, u8, u8),
+    CmykColor { cyan: u8, magenta: u8, yellow: u8, black: u8 }
 }
 
 fn main() {
-    let p = Point {
-        x: 3.0,
-        y: 4.0,
+    // let c = Color::RgbColor(10, 0, 0);
+    let c = Color::CmykColor {
+        cyan: 0,
+        magenta: 0,
+        yellow: 0,
+        black: 255,
     };
 
-    println!("point p is at ({}, {})", p.x, p.y);
+    match c
+    {
+        Color::Red => println!("r"),
+        Color::Green => println!("g"),
+        Color::Blue => println!("b"),
+        Color::RgbColor(0, 0, 0) => println!("black"),
+        Color::RgbColor(r, g, b) => println!("rgb({}, {}, {})", r, g, b),
+        Color::CmykColor { cyan: _, magenta: _, yellow: _, black: 255} => println!("black"),
+        _ => ()
+    }
 }
