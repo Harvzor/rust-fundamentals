36f1ca09f8783bdb45adefc4c093dff61a77cab6
Harvey <harveyjwilliams@aol.co.uk>
2020-10-16 15:55:32 +0200

    3. If statement

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,8 @@
-#![allow(dead_code)]
-
-use std::mem;
-
-// Stack is fast but limited.
-// There's plenty of stack.
-
-struct Point
-{
-    x: f64,
-    y: f64,
-}
-
-fn origin() -> Point
-{
-    Point
-    {
-        x: 0.0,
-        y: 0.0,
-    }
-}
-
-fn stack_and_heap() {
-    // Stack allocated by default.
-    let p1 = origin();
-
-    // Pointer to location on the stack.
-    let p2 = Box::new(origin());
-
-    // Should be 16 bytes since there's 2 f64 vars.
-    println!("p1 takes up {} bytes", mem::size_of_val(&p1));
-
-    // Should be 8 since this is a 8 byte system.
-    println!("p2 takes up {} bytes", mem::size_of_val(&p2));
-
-    // Follow the point, relocate back to the stack in a new var.
-    let p3 = *p2;
-
-    println!("{}", p3.x);
-}
-
 fn main() {
-    stack_and_heap();
+    let temp = 23;
+
+    // Like: `let day = temp > 20 ? "sunny" : "cloudy";`
+    let day = if temp > 20 { "sunny" } else { "cloudy" };
+
+    println!("day is {}", day);
 }
