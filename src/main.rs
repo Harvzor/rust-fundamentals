de7a0b8237166fcc32f92ee51658a6355fec5cc0
Harvey <harveyjwilliams@aol.co.uk>
2020-10-18 21:04:13 +0200

    5. Slices

--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,19 @@
 use std::mem;
 
+// &[i32] = we're taking a slice = we're borrowing a part of an array of i32s
+fn use_slice(slice: &mut[i32]) {
+    println!("first elem = {}, len = {}", slice[0], slice.len());
+
+    slice[0] = 4321;
+}
+
 fn main() {
-    // Like a List in C#, grows dynamically.
-    // Must be mutable, otherwise you can't change it...
-    let mut a =  Vec::new();
+    let mut data = [1, 2, 3, 4, 5];
 
-    a.push(1);
-    a.push(2);
-    a.push(3);
+    // I'm letting the function borrow the 2nd, 3rd and 4th elements.
+    // use_slice(&mut data[1..4]);
 
-    println!("a = {:?}", a);
+    use_slice(&mut data);
 
-    a.push(44);
-
-    println!("a = {:?}", a);
-
-    // Cannot use signed values.
-    // Must be the same size at the machine.
-    // let index: usize = 111;
-
-    // a[index] = 321;
-
-    // Panics.
-    // println!("a[0] = {}", a[index]);
-
-    // Returns Option type.
-    match a.get(3) {
-        Some(x) => println!("a[3] = {}", x),
-        None => println!("error, no such element"),
-    }
-
-    // No idea why the & is there, doesn't seem to change anything.
-    for x in &a {
-        println!("{}", x);
-    }
-
-    // Also returns an Option since we can't be sure there are any elements to pop.
-    let last_elem = a.pop();
-
-    println!("last elem is {:?}, a = {:?}", last_elem, a);
-
-    // Could be a none so this doesn't work.
-    // let Some(_last_value) = a.pop();
-
-    // While `a.pop()` yields a Some, do...
-    while let Some(x) = a.pop() {
-        println!("{}", x);
-    }
+    println!("{:?}", data);
 }
