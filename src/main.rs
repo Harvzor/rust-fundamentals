3b08cd3af540543d2ce9891405dfc8b63fda697c
Harvey <harveyjwilliams@aol.co.uk>
2020-10-25 19:21:29 +0100

    5. Generics

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
-mod pm;
+struct Point<T, V> {
+    x: T,
+    y: V,
+}
+
+struct Line<T> {
+    start: Point<T, T>,
+    end: Point<T, T>,
+}
 
 fn main() {
-    pm::pattern_matching();
+    let a: Point<u16, u16> = Point { x: 0, y: 0 };
+    let b = Point { x: 1.2, y: 3.4 };
+
+    // Doesn't work since Line start and end must be the same type.
+    //let line = Line { start: a, end: b };
 }
