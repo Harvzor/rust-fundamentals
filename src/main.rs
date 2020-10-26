2fc082c76cfe040b66a3085d02e7cb4138fb220a
Harvey <harveyjwilliams@aol.co.uk>
2020-10-26 14:39:38 +0100

    6. Higher-order functions

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,34 @@
-struct Point {
-    x: f64,
-    y: f64,
-}
-
-struct Line {
-    start: Point,
-    end: Point,
-}
-
-impl Line {
-    // len is a method on the Line class
-    // &self is a reference to this class
-    fn len(&self) -> f64 {
-        let dx = self.start.x - self.end.x;
-        let dy = self.start.y - self.end.y;
-
-        (dx*dx+dy*dy).sqrt()
-    }
+fn is_even(x: u32) -> bool {
+    x % 2 == 0
 }
 
 fn main() {
-    let line = Line {
-        start: Point {
-            x: 3.0,
-            y: 3.0,
-        },
-        end: Point {
-            x: 4.0,
-            y: 4.0,
-        },
-    };
-    
-    println!("{}", line.len());
+    let limit = 500;
+    let mut sum = 0;
+
+    for i in 0..
+    {
+        let isq = i*i;
+
+        if isq > limit {
+            break;
+        } else if is_even(isq) {
+            sum += isq;
+        }
+    }
+
+    println!("loop sum = {}", sum);
+
+    let sum2 = (0..)
+        .map(|x| x*x)
+        .take_while(|&x| x < limit)
+        // x is a reference, so we need to dereference
+        .filter(|x| is_even(*x))
+        // fold takes the initial value and the folding function
+        // if we wanted to sum, we should start at 0 for the starter value
+        // if we wanted to multiply, we should start at 1
+        // sum is the value of each fold foreached over, so sum is 0 initially
+        .fold(0, |sum, x| sum + x);
+        
+    println!("higher order function sum = {}", sum2);
 }
