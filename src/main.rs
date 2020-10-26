54a9293dadb1445b592064c351d41b2eaa768f7c
Harvey <harveyjwilliams@aol.co.uk>
2020-10-26 15:01:02 +0100

    6. Traits

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,88 @@
-fn is_even(x: u32) -> bool {
-    x % 2 == 0
+// I guess a trait is an abstract class
+trait Animal {
+    // Static function since it doesn't take &self as a parameter.
+    fn create(name: &'static str) -> Self;
+    // Must be implemented.
+    fn name(&self) -> &'static str;
+    // Doesn't need to be implemented as it as a default implementation.
+    fn talk(&self)
+    {
+        println!("{} cannot talk", self.name());
+    }
+}
+
+struct Human {
+    name: &'static str
+}
+
+struct Cat {
+    name: &'static str
+}
+
+impl Animal for Human {
+    fn create(name: &'static str) -> Human {
+        Human {
+            name: name,
+        }
+    }
+    fn name(&self) -> &'static str {
+        self.name
+    }
+    fn talk(&self) {
+        println!("{} says hello", self.name());
+    }
+}
+
+impl Animal for Cat {
+    fn create(name: &'static str) -> Cat {
+        Cat {
+            name: name,
+        }
+    }
+    fn name(&self) -> &'static str {
+        self.name
+    }
+    fn talk(&self) {
+        println!("{} says meow", self.name());
+    }
+}
+
+trait Summable<T> {
+    fn sum(&self) -> T;
+}
+
+impl Summable<i32> for Vec<i32> {
+    fn sum(&self) -> i32 {
+        // let mut result: i32 = 0;
+
+        // for x in self {
+        //     result += *x;
+        // }
+
+        // result
+
+        return self
+            .iter()
+            .fold(1, |sum, x| sum * x);
+    }
 }
 
 fn main() {
-    let limit = 500;
-    let mut sum = 0;
+    // let h = Human {
+    //     name: "Harvey",
+    // };
+    // The compiler actually chooses to use the Human::create() method since we specified the type.
+    let h: Human = Animal::create("Harvey");
 
-    for i in 0..
-    {
-        let isq = i*i;
+    h.talk();
 
-        if isq > limit {
-            break;
-        } else if is_even(isq) {
-            sum += isq;
-        }
-    }
+    let c = Cat {
+        name: "Cattoo",
+    };
 
-    println!("loop sum = {}", sum);
+    c.talk();
 
-    let sum2 = (0..)
-        .map(|x| x*x)
-        .take_while(|&x| x < limit)
-        // x is a reference, so we need to dereference
-        .filter(|x| is_even(*x))
-        // fold takes the initial value and the folding function
-        // if we wanted to sum, we should start at 0 for the starter value
-        // if we wanted to multiply, we should start at 1
-        // sum is the value of each fold foreached over, so sum is 0 initially
-        .fold(0, |sum, x| sum + x);
-        
-    println!("higher order function sum = {}", sum2);
+    let a = vec![1, 2, 3];
+
+    println!("sum = {}", a.sum());
 }
