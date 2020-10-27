790cbd9d7c688f101c58f25a628478b0786b517f
Harvey <harveyjwilliams@aol.co.uk>
2020-10-27 19:49:26 +0100

    7. Borrowing

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,36 @@
 fn main() {
-    // v owns the memory that is stored
-    // v itself is on the stack, likely a pointer?
-    // v (the data) is on the heap
-    let v = vec![1, 2, 3];
-
-    // v2 is copying the pointer
-    //let v2 = v;
-
-    // Does not compile because v2 now has the pointer,
-    // and rust ensures safety by making sure only one
-    // variable points to the data.
-    //println!("{:?}", v);
-
-    // foo(v) also would borrow the value, not letting us use v anymore.
-    //let foo = |v: Vec<i32>| ();
-    //foo(v);
-
-    let u = 1;
-    let _u2 = u;
-
-    // However this works because with primitive types, the value is copied.
-    // Copying such simple types on the stack is fine because they take up such little memory.
-    println!("u = {}", u);
-
-    // Putting something deliverately on the heap means we must have a pointer here:
-    let u3 = Box::new(1);
-    // Which means a borrow occurs here:
-    let _u4 = u3;
-
-    // So this cannot use a borrowed value:
-    //println!("u3 = {}", *u3);
-
-    // This is an inconvenient way of controller ownership:
-    let print_vector = |x: Vec<i32>| -> Vec<i32> {
+    let print_vector = |x: &Vec<i32>| {
         println!("{:?}", x);
-
-        // Return control of the vector...
-        x
     };
 
-    let vv = print_vector(v);
+    let v = vec![3, 2, 1];
+
+    // & - instead of giving control of v over to the closure,
+    // let it borrow my vector for a while
+    print_vector(&v);
+
+    // This now works fine.
+    println!("v[0] = {}", v[0]);
+
+    let mut a = 40;
+
+    // The tutorial says that the braces must be necessary so that b stops borrowing a but it works fine without too.
+    {
+        // b becomes a mutabale reference to a.
+        let b = &mut a;
+
+        // * - follow the reference
+        *b += 2;
+    }
+
+    println!("a = {}", a);
+
+    let mut z = vec![3, 2, 1];
+
+    for i in &z {
+        println!("i = {}", i);
+
+        // Doesn't work because it would overload the vector.
+        //z.push(5);
+    }
 }
